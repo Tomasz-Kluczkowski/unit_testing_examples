2a9c3d06c14fba90e94c6e80738aff8f5b63cf03
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-10-17 00:28:18 +0100

    Finished writing Fridge class and unit testing examples for it.

--- fridge.py
+++ fridge.py
@@ -1,25 +1,26 @@
+import requests
+
 class Fridge(object):
     """Keeps the brew intact."""
 
     max_load = 20
 
-    def __init__(self, load=0, temp=8, base_speed=500, inverter=None):
+    def __init__(self, inverter, load=0, temp=8, base_speed=500):
         """Start chilling with factory settings."""
 
-        if not inverter:
+        if temp < 1:
+            raise ValueError("Ice cream or beer Sir?")
 
-            raise ValueError("Inverter parameter must be set.")
-
+        self.inverter = inverter
         self.load = load
         self.temp = temp
         self.base_speed = base_speed
-        self.inverter = inverter
 
     # Private method.
     def _fan_speed_factor(self):
         """Calculate fan speed adjustment for the load."""
 
-        _fan_speed_factor = (self.max_load + self.load)/self.max_load
+        _fan_speed_factor = (self.max_load + self.load) / self.max_load
 
         return _fan_speed_factor
 
@@ -27,7 +28,7 @@ class Fridge(object):
     def get_fan_speed_setting(self):
         """Provide fan speed setting to the display PCB."""
 
-        return self.base_speed*self._fan_speed_factor()
+        return self.base_speed * self._fan_speed_factor()
 
     # Perform an incoming command - change temperature.
     def set_temp(self, new_temp):
@@ -35,5 +36,35 @@ class Fridge(object):
 
         self.temp = new_temp
 
+    # Outgoing command - contact the inverter.
+    def set_target_speed(self, target_speed):
+        """Sets new fan target speed."""
 
+        self.inverter.set_target_speed(target_speed)
+
+        if self.get_current_speed() != target_speed:
+
+            raise ValueError("Unable to confirm fan speed change.")
+
+    # Outgoing query.
+    def get_current_speed(self):
+        """Contact inverter to get current fan speed."""
+
+        return self.inverter.get_current_speed()
+
+    # Contacting web based API.
+    def order_beer(self, num_of_bottles):
+        """Contact web based API to order more beer."""
+
+        payload = {"num_bot": num_of_bottles,
+                   "delivery_address": "my_fridge_obviously"}
+        response = requests.post("https://get_more_beer.com", data=payload)
+
+        if response.status_code == 200:
+
+            return response
+
+        else:
+
+            return "incorrect_response: {0}".format(response.status_code)
 
--- tests/test_fridge.py
+++ tests/test_fridge.py
@@ -12,7 +12,7 @@ from fridge import Fridge
 def loaded_fridge():
     """Create Fridge object for testing."""
 
-    _fridge = Fridge(load=10, temp=10, base_speed=1000, inverter="Inverter")
+    _fridge = Fridge("INV4403PH", load=10, temp=10, base_speed=1000)
 
     return _fridge
 
@@ -22,41 +22,44 @@ def loaded_fridge():
 def loaded_fridge_param(request):
     """Create Fridge object for testing with adjustable attributes."""
 
-    load, temp, base_speed, inverter = request.param
+    inverter, load, temp, base_speed = request.param
 
-    _fridge = Fridge(load=load, temp=temp, base_speed=base_speed,
-                     inverter=inverter)
+    _fridge = Fridge(inverter, load, temp, base_speed)
 
     return _fridge
 
 
 @pytest.fixture(scope="function")
-def mock_inverter(current_speed):
+def mock_inverter(request):
     """Substitute our inverter object with a fake one.
     Allows setting current speed to test multiple conditions."""
 
+    current_speed = request.param
+
     _inverter = mock.Mock()
-    _inverter.get_current_speed.side_effect = [current_speed]
+    _inverter.get_current_speed.return_value = current_speed
 
     return _inverter
 
 
+# This is just the simplest example. Normally if init method is so simple we
+# would not test it.
 def test_instantiation(loaded_fridge):
     """Test initialization of the Fridge class."""
 
     assert loaded_fridge.load == 10
     assert loaded_fridge.temp == 10
     assert loaded_fridge.base_speed == 1000
-    assert loaded_fridge.inverter == "Inverter"
+    assert loaded_fridge.inverter == "INV4403PH"
 
 
-def test_raises_exception_if_no_inverter_parameter_set():
-    """Checks if exception is raised if Fridge class is instantiated with no
-    inverter parameter."""
+def test_raises_exception_when_temp_too_low():
+    """Checks if exception is raised if Fridge class is instantiated with temp
+    below 1."""
 
-    with pytest.raises(ValueError, message="Inverter parameter must be set."):
+    with pytest.raises(ValueError, message="Ice cream or beer Sir?"):
 
-        no_inverter_fridge = Fridge()
+        wrong_temp_fridge = Fridge("INV2401PH", temp=0)
 
 
 # Test incoming query - notice that due to dependency on calculation in
@@ -67,11 +70,11 @@ def test_get_fan_speed_setting(loaded_fridge):
     assert loaded_fridge.get_fan_speed_setting() == 1500
 
 
-# (load, temp, base_speed, inverter_type, fan_speed_setting)
-fridge_parameters = [((0, 5, 500, "INV2401PH"), 500),
-                     ((5, 0, 100, "INV2403PH"), 125),
-                     ((15, 15, 700, "INV4401PH"), 1225),
-                     ((3, 9, 300, "INV4403PH"), 345)]
+# ((inverter, load, temp, base_speed), fan_speed_setting)
+fridge_parameters = [(("INV2401PH", 0, 5, 500), 500),
+                     (("INV2403PH", 5, 2, 100), 125),
+                     (("INV4401PH", 15, 15, 700), 1225),
+                     (("INV4403PH", 3, 9, 300), 345)]
 
 
 # Indirect parametrization - passing arguments to fixtures.
@@ -89,7 +92,7 @@ temp_data = [(10, 10),
              (6, 6)]
 
 
-# Simple parametrization of test itself.
+# Simple parametrization of test variables.
 # Test an incoming command (from the display PCB).
 @pytest.mark.parametrize("test_temp, expected", temp_data)
 def test_set_temp(loaded_fridge, test_temp, expected):
@@ -98,14 +101,81 @@ def test_set_temp(loaded_fridge, test_temp, expected):
     loaded_fridge.set_temp(test_temp)
 
     assert loaded_fridge.temp == expected
-#
-#
-# # # Test outgoing command with a correct response from dependencies.
-# # def test_set_target_speed(loaded_fridge, mock_inverter):
-# #     """Test behavior of fridge when current speed matches set speed."""
-# #
-# #     inverter = mock_inverter
-# #     loaded_fridge.set_target_speed(1200)
+
+test_data = [(1000, 1000), (1200, 1200), (1300, 1300), (1400, 1400)]
+# Test outgoing command was sent out to the inverter and a correct response
+#  was received.
+@pytest.mark.parametrize("mock_inverter, expected", test_data,
+                         indirect=["mock_inverter"])
+def test_set_target_speed(loaded_fridge, mock_inverter, expected):
+    """Test behavior of fridge when current speed matches set speed."""
+
+    loaded_fridge.inverter = mock_inverter
+    loaded_fridge.set_target_speed(expected)
+
+    mock_inverter.set_target_speed.assert_called_once_with(expected)
+
+
+# Test outgoing command raises exception on incorrect response from inverter.
+@pytest.mark.parametrize("mock_inverter", [1000, 1200, 1300, 1400],
+                         indirect=["mock_inverter"])
+def test_raises_exception_if_curr_spd_not_target(loaded_fridge, mock_inverter):
+    """Test behavior of fridge when current speed differs from set speed."""
+
+    with pytest.raises(ValueError,
+                       message="Unable to confirm fan speed change."):
+
+        loaded_fridge.inverter = mock_inverter
+        loaded_fridge.set_target_speed(1500)
+
+
+class TestApiCalls(object):
+    """Test contacting web based API."""
+
+    URL = "https://get_more_beer.com"
+    num_of_bottles = 20
+    payload = {"num_bot": num_of_bottles,
+               "delivery_address": "my_fridge_obviously"}
+    reply = [{"order_status": "confirmed", "amount_ordered": num_of_bottles}]
+
+    @pytest.fixture(scope="function")
+    def mock_response_200(self):
+        """Provide a substitute for web based API call."""
+
+        mock_response = mock.Mock()
+        mock_response.return_value.status_code = 200
+        mock_response.return_value.json.return_value = self.reply
+
+        return mock_response
+
+    @pytest.fixture(scope="function")
+    def mock_response_400(self):
+        """Provide a substitute for web based API call."""
+
+        mock_response = mock.Mock()
+        mock_response.return_value.status_code = 400
+
+        return mock_response
+
+    # Test contacting external web based API with a response 200.
+    def test_order_beer_response_200(self, loaded_fridge, mock_response_200, monkeypatch):
+        """Test contacting web based API."""
+
+        monkeypatch.setattr("fridge.requests.post", mock_response_200)
+
+        assert loaded_fridge.order_beer(self.num_of_bottles).status_code == 200
+        mock_response_200.assert_called_with(self.URL, data=self.payload)
+
+        assert loaded_fridge.order_beer(self.num_of_bottles).json() == self.reply
+
+    # Test contacting external web based API with a response 400.
+    def test_order_beer_response_400(self, loaded_fridge, mock_response_400, monkeypatch):
+        """Test contacting web based API."""
+
+        monkeypatch.setattr("fridge.requests.post", mock_response_400)
+
+        assert loaded_fridge.order_beer(self.num_of_bottles) == "incorrect_response: 400"
+        mock_response_400.assert_called_with(self.URL, data=self.payload)
 
 
 if __name__ == "__main__":
