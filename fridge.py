33b691c7b299f699d4a4217ab424cc0c2f4771bb
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-10-28 18:02:00 +0100

    Modified fridge.py and test_fridge.py, removed indirect experiments and wrapper example.

--- fridge.py
+++ fridge.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class Fridge(object):
     """Keeps the brew intact."""
 
@@ -11,36 +12,42 @@ class Fridge(object):
         if temp < 1:
             raise ValueError("Ice cream or beer Sir?")
 
-        self.inverter = inverter
         self.load = load
         self.temp = temp
-        self.base_speed = base_speed
+        self._inverter = inverter
+        self._base_speed = base_speed
 
     # Private method.
     def _fan_speed_factor(self):
         """Calculate fan speed adjustment for the load."""
 
         _fan_speed_factor = (self.max_load + self.load) / self.max_load
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
         return _fan_speed_factor
 
     # Answer an incoming query (from the display PCB).
     def get_fan_speed_setting(self):
         """Provide fan speed setting to the display PCB."""
 
-        return self.base_speed * self._fan_speed_factor()
+        return self._base_speed * self._fan_speed_factor()
 
-    # Perform an incoming command - change temperature.
+    # Answer an incoming query (from the display PCB).
+    def get_temp(self):
+        """Provide temperature reading for the display PCB."""
+
+        return self.temp
+
+    # Perform an incoming command - change the temperature.
     def set_temp(self, new_temp):
         """Set new target temperature."""
 
         self.temp = new_temp
 
-    # Outgoing command - contact the inverter.
+    # Outgoing command - contact the _inverter.
     def set_target_speed(self, target_speed):
         """Sets new fan target speed."""
 
-        self.inverter.set_target_speed(target_speed)
+        self._inverter.set_target_speed(target_speed)
 
         if self.get_current_speed() != target_speed:
 
@@ -48,9 +55,9 @@ class Fridge(object):
 
     # Outgoing query.
     def get_current_speed(self):
-        """Contact inverter to get current fan speed."""
+        """Contact _inverter to get current fan speed."""
 
-        return self.inverter.get_current_speed()
+        return self._inverter.get_current_speed()
 
     # Contacting web based API.
     def order_beer(self, num_of_bottles):
--- tests/test_fridge.py
+++ tests/test_fridge.py
@@ -5,10 +5,10 @@ from fridge import Fridge
 
 
 # Scope argument in fixture allows running it once per event.
-# function, module, class, session as argument are allowed.
+# function (default), module, class, session as argument are allowed.
 # autouse=True runs the fixture after launching tests automatically without
 # having to "use it up" in a test.
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def loaded_fridge():
     """Create Fridge object for testing."""
 
@@ -18,7 +18,7 @@ def loaded_fridge():
 
 
 # This fixture allows parametrizing it from the test.
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def loaded_fridge_param(request):
     """Create Fridge object for testing with adjustable attributes."""
 
@@ -31,7 +31,7 @@ def loaded_fridge_param(request):
 
 @pytest.fixture(scope="function")
 def mock_inverter(request):
-    """Substitute our inverter object with a fake one.
+    """Substitute our _inverter object with a fake one.
     Allows setting current speed to test multiple conditions."""
 
     current_speed = request.param
@@ -42,15 +42,13 @@ def mock_inverter(request):
     return _inverter
 
 
-# This is just the simplest example. Normally if init method is so simple we
-# would not test it.
+# We will prove later on that thanks to indirect testing we can get rid of this
+# test function altogether.
 def test_instantiation(loaded_fridge):
     """Test initialization of the Fridge class."""
 
     assert loaded_fridge.load == 10
     assert loaded_fridge.temp == 10
-    assert loaded_fridge.base_speed == 1000
-    assert loaded_fridge.inverter == "INV4403PH"
 
 
 def test_raises_exception_when_temp_too_low():
@@ -70,7 +68,7 @@ def test_get_fan_speed_setting(loaded_fridge):
     assert loaded_fridge.get_fan_speed_setting() == 1500
 
 
-# ((inverter, load, temp, base_speed), fan_speed_setting)
+# ((_inverter, load, temp, _base_speed), expected fan_speed_setting)
 fridge_parameters = [(("INV2401PH", 0, 5, 500), 500),
                      (("INV2403PH", 5, 2, 100), 125),
                      (("INV4401PH", 15, 15, 700), 1225),
@@ -80,7 +78,7 @@ fridge_parameters = [(("INV2401PH", 0, 5, 500), 500),
 # Indirect parametrization - passing arguments to fixtures.
 @pytest.mark.parametrize("loaded_fridge_param, expected", fridge_parameters,
                          indirect=["loaded_fridge_param"])
-def test_instantiation_multiple(loaded_fridge_param, expected):
+def test_get_fan_speed_setting_multiple(loaded_fridge_param, expected):
     """Test initialization of the Fridge class."""
 
     assert loaded_fridge_param.get_fan_speed_setting() == expected
@@ -102,31 +100,37 @@ def test_set_temp(loaded_fridge, test_temp, expected):
 
     assert loaded_fridge.temp == expected
 
+
+def test_get_temp(loaded_fridge):
+    """Test getting temperature reading for the display PCB."""
+
+    assert loaded_fridge.get_temp() == loaded_fridge.temp
+
 test_data = [(1000, 1000), (1200, 1200), (1300, 1300), (1400, 1400)]
-# Test outgoing command was sent out to the inverter and a correct response
+# Test outgoing command was sent out to the _inverter and a correct response
 #  was received.
 @pytest.mark.parametrize("mock_inverter, expected", test_data,
                          indirect=["mock_inverter"])
-def test_set_target_speed(loaded_fridge, mock_inverter, expected):
+def test_set_target_speed(mock_inverter, expected):
     """Test behavior of fridge when current speed matches set speed."""
 
-    loaded_fridge.inverter = mock_inverter
-    loaded_fridge.set_target_speed(expected)
+    fridge = Fridge(mock_inverter)
+    fridge.set_target_speed(expected)
 
     mock_inverter.set_target_speed.assert_called_once_with(expected)
 
 
-# Test outgoing command raises exception on incorrect response from inverter.
+# Test outgoing command raises exception on incorrect response from _inverter.
 @pytest.mark.parametrize("mock_inverter", [1000, 1200, 1300, 1400],
                          indirect=["mock_inverter"])
-def test_raises_exception_if_curr_spd_not_target(loaded_fridge, mock_inverter):
+def test_raises_exception_if_curr_spd_not_target(mock_inverter):
     """Test behavior of fridge when current speed differs from set speed."""
 
     with pytest.raises(ValueError,
                        message="Unable to confirm fan speed change."):
 
-        loaded_fridge.inverter = mock_inverter
-        loaded_fridge.set_target_speed(1500)
+        fridge = Fridge(mock_inverter)
+        fridge.set_target_speed(1500)
 
 
 class TestApiCalls(object):
@@ -163,7 +167,7 @@ class TestApiCalls(object):
 
         monkeypatch.setattr("fridge.requests.post", mock_response_200)
 
-        assert loaded_fridge.order_beer(self.num_of_bottles).status_code == 200
+        loaded_fridge.order_beer(self.num_of_bottles)
         mock_response_200.assert_called_with(self.URL, data=self.payload)
 
         assert loaded_fridge.order_beer(self.num_of_bottles).json() == self.reply
